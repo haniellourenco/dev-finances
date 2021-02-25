f0470298aca8e2ca3128a540e01755d179ab2955
Haniel Lourenço <hanielohn2016@gmail.com>
2021-02-25 04:44:28 -0300

    correcao bug formatamount

--- scripts.js
+++ scripts.js
@@ -108,9 +108,9 @@ const DOM = {
 
 const Utils = {
     formatAmount(value){
-        value = Number(value) * 100
+        value = value * 100
 
-        return value
+        return Math.round(value)
     },
 
     formatDate(date) {
